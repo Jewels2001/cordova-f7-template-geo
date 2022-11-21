bb0d472f3ae4085015db99f93dcf59945b0ba3ec
Jewels2001 <juliewq25@gmail.com>
2022-11-21 11:44:02 -0700

    Implement geolocation plugin

--- www/js/index.js
+++ www/js/index.js
@@ -24,6 +24,59 @@ $$(document).on('page:init', '.page[data-name="page2"]', function () {
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
+    var geoOpts = { 
+        enableHighAccuracy: true
+    };
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOpts);
+
+    $("#geobtn").on("click", function() {
+        console.log("clicked!");
+        navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOpts);
+    })
+    
+
+    function geoSuccess(position) {
+        console.log(position);
+        var lat = position.coords.latitude;
+        var lng = position.coords.longitude;
+        $('#currentPos').html(lat + ', ' + lng);
+    }
+
+    function geoError(error) {
+        alert(error.message);
+    }
+
+
+    var watchID, map, marker;
+    $("#startWatch").on("click", function() {
+        console.log("Start watching hehe")
+        watchID = navigator.geolocation.watchPosition(geoSuccess, geoError, geoOpts);
+        $(this).hide()
+        $("#stopWatch").show();
+    });
+
+    $("#stopWatch").on("click", function() {
+        console.log("Stop watching :,(")
+        navigator.geolocation.clearWatch(watchID);
+        $(this).hide()
+        $("#startWatch").show()
+    })
+
+    // var map = new google.maps.Map(document.getElementById('map'), {
+    //     zoom: 10,
+    //     center: {
+    //         lat: position.coords.latitude,
+    //         lng: position.coords.longitude
+    //     }
+    // });
+
+    // var marker = new google.maps.Marker({
+    //     position: {
+    //         lat: position.coords.latitude,
+    //         lng: position.coords.longitude
+    // },
+    //     map: map
+    // });
 
     // Cordova is now initialized. Have fun!
 
